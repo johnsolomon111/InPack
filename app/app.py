e4f5e3bbe0cea95656949078e5f0ba6d5d7a2ee4
unknown <johnbenedict.solomon@g.msuiit.edu.oh>
2019-10-11 03:01:11 +0800

    Initial Login UI

--- app/app.py
+++ app/app.py
@@ -5,4 +5,8 @@ from flask import render_template
 
 @server.route('/', methods=["GET"])
 def index():
-	return render_template('index.html')
+	return render_template('index.html', title="Inpack")
+
+@server.route('/login', methods=["GET"])
+def login_with_password():
+	return render_template('login.html', title="Log In with Password")

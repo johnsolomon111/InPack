39d34c5663af06d5d7b119fb1716f120b5baa48a
mariabeaalyssa <beaalyssa1@gmail.com>
2019-10-17 11:52:43 +0800

    css fixed

--- app/app.py
+++ app/app.py
@@ -1,8 +1,21 @@
 from app import *
-
+from flask import Flask, render_template, url_for, flash, redirect, session
+import os 
 from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
 
 
-@server.route('/', methods=["GET"])
+@app.route('/', methods=["GET","POST"])
 def index():
 	return render_template('index.html')
+
+
+@app.route('/landing_page',methods=['GET','POST'])
+def landing():
+    return render_template('landingpage.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)

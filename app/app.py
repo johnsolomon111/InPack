6b90f5a19973d529782cc553ee6d397e1057953a
unknown <johnbenedict.solomon@g.msuiit.edu.oh>
2019-10-24 10:53:34 +0800

    Modifications in views

--- app/app.py
+++ app/app.py
@@ -1,19 +1,45 @@
 from app import *
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 @server.route('/', methods=["GET","POST"])
 def index():
 	form = LoginForm()
-	if form.validate_on_submit():
-		return 'username: {} rfid: {}'.format(form.username.data, form.rfid.data)
+	if current_user.is_authenticated is True:
+		return redirect(url_for('dashboard'))
+	elif form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.rfid, form.rfid.data):
+				login_user(user)
+				return redirect(url_for('dashboard'))
+			else:
+				flash('Invalid username or password')
+				return render_template('index.html', form=form, title="Inpack")
+		else:
+			return render_template('index.html', form=form, title="Inpack")
 	return render_template('index.html', title="Inpack", form=form)
 
 @server.route('/login', methods=["GET", "POST"])
 def login_with_password():
 	form = LoginForm()
-	if form.validate_on_submit():
-		return 'username: {} password: {}'.format(form.username.data, form.password.data)
+	if current_user.is_authenticated is True:
+		return redirect(url_for('dashboard'))
+	elif form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user)
+				return redirect(url_for('dashboard'))
+			else:
+				flash('Invalid username or password')
+				return render_template('login.html', form=form, title="Log In with Password")
+		else:
+			return render_template('login.html', title="Log In with Password", form=form)
 	return render_template('login.html', title="Log In with Password", form=form)
 
 @server.route('/signup', methods=["GET", "POST"])
@@ -24,10 +50,17 @@ def signup():
 		dbase.session.add(new_user)
 		dbase.session.commit()
 
-		# return 'contact: {} firstname: {} lastname: {} email: {} username: {} password: {} confirm: {} rfid: {}'.format(form.contact.data, form.firstname.data, form.lastname.data, form.email.data, form.username.data, form.password.data, form.confirm.data, form.rfid.data)
+		user = User.query.filter_by(username=form.username.data).first()
+		login_user(user)
 		return redirect(url_for('dashboard'))
 	return render_template('signup.html', title="Get Started!", form=form)
 
 @server.route('/dashboard', methods=["GET", "POST"])
+@login_required
 def dashboard():
 	return render_template('dashboard.html', title="Dashboard")
+
+@server.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))

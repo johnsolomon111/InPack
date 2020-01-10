03184acf6947fc6f1952837b333ec2b3c971190d
greggyjane <greggyjane.gac-ang@g.msuiit.edu.ph>
2020-01-10 13:20:10 +0800

    fifth commit

--- app/app.py
+++ app/app.py
@@ -1,9 +1,6 @@
 from app import *
 from sqlalchemy import or_
-<<<<<<< HEAD
 from sqlalchemy import and_
-=======
->>>>>>> 248a0241aaa28639311c3dd373413adc69e8dcfc
 
 from flask import render_template, request, redirect, url_for, flash, request
 @login_manager.user_loader
@@ -14,21 +11,13 @@ def load_user(user_id):
 def index():
 	form = LoginForm()
 	if current_user.is_authenticated is True:
-<<<<<<< HEAD
 		return redirect(url_for('dashboard', page_num=1))
-=======
-		return redirect(url_for('dashboard'))
->>>>>>> 248a0241aaa28639311c3dd373413adc69e8dcfc
 	elif form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
 		if user:
 			if check_password_hash(user.rfid, form.rfid.data):
 				login_user(user)
-<<<<<<< HEAD
 				return redirect(url_for('dashboard', page_num=1))
-=======
-				return redirect(url_for('dashboard'))
->>>>>>> 248a0241aaa28639311c3dd373413adc69e8dcfc
 			else:
 				flash('Invalid username or password')
 				return render_template('index.html', form=form, title="InPack")
@@ -100,7 +89,6 @@ def dashboard(page_num):
 @login_required
 def add_item():
 	form = ItemForm()
-<<<<<<< HEAD
 	if form.validate_on_submit():
 		category_name = request.form['category_name']
 		new_item = Item(form.item_name.data, category_name, form.quantity.data, current_user.college_name, 'Available')
@@ -198,105 +186,6 @@ def dashboard_users():
 	cat = Category.query.filter_by(college_name=current_user.college_name)
 	if form.validate_on_submit():
 		category_name = request.form['category_name']
-=======
-	if form.validate_on_submit():
-		category_name = request.form['category_name']
-		new_item = Item(form.item_name.data, category_name, form.quantity.data, current_user.college_name, 'Available')
-		dbase.session.add(new_item)
-		dbase.session.commit()
-	return redirect(url_for('dashboard', page_num=1))
-
-@server.route('/borrow/item/<int:id>',methods=["POST","GET"])
-@login_required
-def borrow_item(id):
-	item = Item.query.filter_by(item_id=id).first()
-	form3 = BorrowForm()
-	borrower = Borrower.query.all()
-	form4 = BorrowerForm()
-
-
-	if item.quantity >= form3.quantity.data:
-		
-		if form3.validate_on_submit():
-			item.quantity = item.quantity - form3.quantity.data
-			new_borrow = BorrowedItem(item.item_name, item.item_id, form3.quantity.data, current_user.college_name)
-			dbase.session.add(new_borrow)
-			dbase.session.commit()	
-
-		if request.method == 'POST':
-			borrow_fname  = request.form['borrow_fname']
-			borrow_lname = request.form['borrow_lname']
-			borrow_idno = request.form['borrow_idno']
-			borrow_college = request.form['borrow_college']
-			borrow_course = request.form['borrow_course']
-			borrow_status = ['Borrowed']
-			borrower= Borrower(borrow_fname=borrow_fname, borrow_lname=borrow_lname, borrow_idno=borrow_idno,borrow_college=borrow_college, borrow_course=borrow_course, item_id=item.item_id, quantity=form3.quantity.data)
-			dbase.session.add(borrower)
-			dbase.session.commit()		
-		return redirect(url_for('dashboard', page_num=1))
-	return redirect(url_for('dashboard', page_num=1))
-
-@server.route('/view/borrowed_items', methods=["GET", "POST"])
-@login_required
-def borrowed_items():
-	user = current_user
-	college = College.query.filter_by(college_name=current_user.college_name).first()
-	borrow = Borrower.query.all()
-	item = Item.query.all()
-	return render_template('viewborrowed.html', item = item, user=user, college=college, borrow=borrow, title='Borrowed Items | InPack')
-
-@server.route('/item/<item_id>', methods=["GET", "POST"])
-@login_required
-def view_item(item_id):
-	user = current_user
-	college = College.query.filter_by(college_name=current_user.college_name).first()
-	item = Item.query.get_or_404(item_id)
-	items = Item.query.filter_by(college_name=current_user.college_name)
-	form = ItemForm()
-	cat = Category.query.filter_by(college_name=current_user.college_name)
-
-
-	return render_template('viewitem.html', cat = cat, form = form, items=items, item = item, user=user, college=college, title='Items | InPack')
-
-@server.route('/update/item/<int:item_id>', methods=["GET", "POST"])
-@login_required
-def update_item(item_id):
-	user = current_user
-	college = College.query.filter_by(college_name=current_user.college_name).first()
-	item = Item.query.get_or_404(item_id)
-	items = Item.query.filter_by(college_name=current_user.college_name)
-	form = ItemForm()
-	cat = Category.query.filter_by(college_name=current_user.college_name)
-
-	if form.validate_on_submit():
-		item.item_name = form.item_name.data
-		category_name = request.form['category_name']
-		item.quantity = form.quantity.data
-		dbase.session.commit()
-		flash('Your post has been updated!', 'success')
-		return redirect(url_for('view_item', item_id=item.item_id))
-	return redirect(url_for('view_item', item_id=item.item_id))
-
-@server.route('/delete/item/<int:id>', methods=["POST", "GET"])
-@login_required
-def delete_item(id):
-	del_item = Item.query.filter_by(item_id=id).first()
-	dbase.session.delete(del_item)
-	dbase.session.commit()
-	return redirect(url_for('dashboard', page_num=1))
-
-@server.route('/dashboard/users', methods=["GET", "POST"])
-@login_required
-def dashboard_users():
-	user = current_user
-	college = College.query.filter_by(college_name=current_user.college_name).first()
-	data = User.query.filter_by(college_name=current_user.college_name)
-	form = RegistrationForm()
-	form2 = CategoryForm()
-	cat = Category.query.filter_by(college_name=current_user.college_name)
-	if form.validate_on_submit():
-		category_name = request.form['category_name']
->>>>>>> 248a0241aaa28639311c3dd373413adc69e8dcfc
 		new_user = User(form.contact.data, form.firstname.data, form.lastname.data, form.email.data, form.username.data, form.password.data, form.rfid.data, category_name,current_user.college_name)
 		dbase.session.add(new_user)
 		dbase.session.commit()
@@ -406,7 +295,6 @@ def college():
 	return render_template('college.html', title='Add College | InPack', form=form)
 
 @server.route('/search', methods=['GET', 'POST'])
-<<<<<<< HEAD
 @login_required
 def search():
 	page_num = 1
@@ -441,17 +329,6 @@ def search():
 
 		return render_template('dashboard.html', title="Dashboard | List",query=query, search_form=search_form, x=x,form3=form3,borrower=borrower,form4=form4,threads=threads,t_borrow=t_borrow,t_data=t_data,cat=cat,form=form,form2=form2, user=user, college=college)    
 	return redirect(url_for('dashboard', page_num=1))
-=======
-def search():
-    search_form = SearchForm()
-    form = ItemForm()
-    query = search_form.search.data
-    if search_form.validate_on_submit():
-        qry = dbase.session.query(Item).filter(or_( Item.item_name.ilike(query), Item.category.ilike(query) ) )
-        items = qry.all()
-        return render_template('dashboard.html',form=form, search_form=search_form, query=query, items=items)      
-    return redirect(url_for('dashboard'))
->>>>>>> 248a0241aaa28639311c3dd373413adc69e8dcfc
 
 
 @server.route('/logout')
